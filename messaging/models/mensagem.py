da55fb490613c748b9608f8147b647f5be507c16
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-10 18:18:18 -0300

    fix(messaging): deixando as importações Aluno e Professor em uma unica linha

--- messaging/models/mensagem.py
+++ messaging/models/mensagem.py
@@ -1,7 +1,6 @@
 from django.db import models
 from courses.models import Matricula
-from accounts.models import Professor
-from accounts.models import Aluno
+from accounts.models import Aluno, Professor
 from django.contrib.auth.models import User
 from datetime import date 
 
