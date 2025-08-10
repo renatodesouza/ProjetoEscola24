b78a96cdeae4cce205d1fa920f6eb4fdcf71be0c
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-10 09:58:53 -0300

    feat: osseguintes apps foram registrados no Installed_Apps accounts, activities, courses, messaging

--- setup/settings.py
+++ setup/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'accounts',
+    'activities',
+    'courses',
+    'messaging',
     'silk',
     'django.contrib.admin',
     'django.contrib.auth',
