8ea0f464fbed0ce6802d2bec30cbe41baba5a076
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-10 09:56:47 -0300

    feat: Adicionada novas rotas para os apps accounts, activities, courses, messaging

--- setup/urls.py
+++ setup/urls.py
@@ -7,5 +7,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('activities/', include('activities.urls')),
+    path('courses/', include('courses.urls')),
+    path('messaging/', include('messaging.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
