3284df0c3b735930123723c2c757247be5d7a4c8
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-16 17:36:37 -0300

    feat(setup): rota para o app core adicionada no setup/urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
+    path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
     path('activities/', include('activities.urls')),
     path('courses/', include('courses.urls')),
