f3a8cbd7fffd2c4b7be94a00218b9de3100150a0
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-16 10:15:31 -0300

    refactor(messaging): movendo a classe MensagemAdmin de app/admin.py para messaging/admin.py

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Mensagem
+from django.db.models import Q
 
-# Register your models here.
+
+@admin.register(Mensagem)
+class MensagemAdmin(admin.ModelAdmin):
+
+    list_display = ('remetente', 'destinatario', 'mensagem', 'dt_envio')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        
+        return queryset.filter(Q(remetente=request.user) | Q(destinatario=request.user))
