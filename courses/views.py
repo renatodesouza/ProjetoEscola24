8a519c2357f5ae9701c64f360508b9eecf7b9a1f
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-16 12:10:50 -0300

    refactor(courses): CursosView movida de app/views.py para courses/views.py

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Curso, Turma
 
-# Create your views here.
+
+class CursoView(DetailView):
+    template_name = 'courses/curso.html'
+    model = Curso
+    context_object_name = 'curso'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cursos'] = Curso.objects.all()
+        curso = self.get_object()
+        turmas = Turma.objects.filter(curso__nome=curso, turma='TURMA A')
+
+        disciplinas_por_semestre = [
+            {'semestre':turma.semestre,
+             'disciplinas':turma.disciplina.all()}
+             for turma in turmas
+        ]
+
+        context['disciplinas_por_semestre'] = disciplinas_por_semestre
+
+        return context
