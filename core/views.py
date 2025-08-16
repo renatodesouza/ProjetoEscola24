6c9c46853e63e61c81ed9bbe38b156de11c135b1
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-16 12:09:13 -0300

    refactor(core): HomeView movida de app/views.py para core/views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from courses.models import Curso
 
 
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cursos'] = Curso.objects.order_by('?')[:3]
+        context['list_cursos'] = Curso.objects.all()
+        
+        return context
