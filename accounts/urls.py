67b78484886f075bb458134ce91fae8dd2899025
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-08-16 10:33:31 -0300

    refactor(accounts): rota para ProfessorView movida de app/urls.py para accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AlunoView
+from .views import AlunoView, ProfessorView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('aluno/<int:pk>/', AlunoView.as_view(), name='aluno'),
+    path('professor/<int:pk>/', ProfessorView.as_view, name='professor'),
 ]
